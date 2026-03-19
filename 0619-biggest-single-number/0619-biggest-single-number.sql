c6e2dbdd98fbcc34a03742bdbb4b8a932f3d74c3
U-D-AY <chilamkurthiudaysrinadh@gmail.com>
2026-03-19 20:06:44 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,6 +1,5 @@
-select max(num) as num from mynumbers 
-where num not in (
+select max(num) as num from (
     select num from mynumbers 
     group by 
-    num having count(num)>1
-)
+    num having count(num)=1
+) as maxy
