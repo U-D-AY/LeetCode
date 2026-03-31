a0bb20e0eda7022650e860945bd7ccc680755261
U-D-AY <chilamkurthiudaysrinadh@gmail.com>
2026-03-31 21:55:10 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1280-students-and-examinations/1280-students-and-examinations.sql
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -1,7 +1,7 @@
 select s.student_id, s.student_name, sub.subject_name, 
 count(e.student_id) as attended_exams
 FROM Students s
-CROSS JOIN Subjects sub
+JOIN Subjects sub
 LEFT JOIN Examinations e 
     ON s.student_id = e.student_id 
     AND sub.subject_name = e.subject_name
