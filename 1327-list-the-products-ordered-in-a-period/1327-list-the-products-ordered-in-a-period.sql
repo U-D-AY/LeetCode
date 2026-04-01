eefcdc710da6d4cf5859055b019a23c6e4052978
U-D-AY <chilamkurthiudaysrinadh@gmail.com>
2026-04-01 21:09:37 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
+++ 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
@@ -1,4 +1,5 @@
 select p.product_name, sum(o.unit) as unit 
 from products p inner join orders o on p.product_id = o.product_id
-where o.order_date>='2020-02-01' and o.order_date<='2020-02-29'
-group by p.product_id having sum(unit)>99
+where o.order_date between'2020-02-01' and '2020-02-29'
+group by p.product_id 
+having sum(unit)>99
