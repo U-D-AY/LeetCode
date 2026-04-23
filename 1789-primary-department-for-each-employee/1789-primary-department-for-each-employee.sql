3d2c63df758ceefa02eae358bb6640d45f034f81
U-D-AY <chilamkurthiudaysrinadh@gmail.com>
2026-04-23 22:06:23 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -1,8 +1,8 @@
 select employee_id, 
-case
-    when count(*)>1 then
-        max(case  when primary_flag = 'Y' then department_id end)
-    else max(department_id )
-    end as department_id
+    coalesce(
+        max(case  when primary_flag = 'Y' then department_id end),
+        max(department_id )
+     ) as department_id
+
 from employee 
 group by employee_id
