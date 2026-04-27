1bc8a14af37aebffbb807643830feed3133fbdce
U-D-AY <chilamkurthiudaysrinadh@gmail.com>
2026-04-27 22:36:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,6 +1,16 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String[] words = s.trim().split("\\s+");
-        return words[words.length -1].length();
+        int i = s.length()-1;
+        int len = 0;
+
+        while(i >= 0 && s.charAt(i)==' ')
+            i--;
+
+        while(i>=0 && s.charAt(i) != ' '){
+            i--;
+            len++;
+        }
+
+        return len;
     }
 }
