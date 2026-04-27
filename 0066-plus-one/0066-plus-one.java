a715af17a1395882bd59b7a89a013f697175fcef
U-D-AY <chilamkurthiudaysrinadh@gmail.com>
2026-04-27 22:52:25 +0530

    Time: 0 ms (100%), Space: 42.9 MB (98.59%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int len = digits.length - 1;
+
+        for(int i = len; i >= 0; i--){
+            if(digits[i] < 9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        int[] res = new int[digits.length + 1];
+        res[0] = 1;
+        return res;
+    }
+}
