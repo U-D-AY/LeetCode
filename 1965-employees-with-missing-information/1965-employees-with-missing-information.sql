6d4c86acb331e7a3e96586fc9375cdc9c00fecdf
U-D-AY <chilamkurthiudaysrinadh@gmail.com>
2026-04-29 16:32:22 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -1,8 +1,12 @@
 select e.employee_id 
-from employees e left join salaries s on  e.employee_id = s.employee_id
+from employees e 
+left join salaries s on  e.employee_id = s.employee_id
 where s.employee_id is null
+
 union all
+
 select s.employee_id 
-from salaries s left join employees e on e.employee_id = s.employee_id
+from salaries s 
+left join employees e on e.employee_id = s.employee_id
 where e.employee_id is null
 order by employee_id
