1a849e159cf9b2641100a5b71d20ea1f69ffc0f6
U-D-AY <chilamkurthiudaysrinadh@gmail.com>
2026-05-10 15:38:42 +0530

    Time: 262 ms (53.88%), Space: 0B (100%) - LeetHub

--- 0620-not-boring-movies/0620-not-boring-movies.sql
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -1,1 +1,4 @@
-select * from cinema where id&1=1 and description <> 'boring' order by rating DESC
+select * from cinema
+where id%2 <> 0 and description <> 'boring'
+order by rating desc
+#select * from cinema where id&1=1 and description <> 'boring' order by rating DESC
