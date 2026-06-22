3b6c0420fbf9eed6f9ee1a9bcabfc793edcb4f20
U-D-AY <chilamkurthiudaysrinadh@gmail.com>
2026-06-22 14:29:36 +0530

    Time: 5 ms (9.33%), Space: 19.3 MB (75.19%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -1,4 +1,10 @@
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
         c = Counter(text)
-        return min(c.get(char, 0) // req for char, req in {'b':1, 'a':1, 'l':2, 'o':2, 'n':1}.items())
+        return min(
+            c['b'],
+            c['a'],
+            c['l']//2,
+            c['o']//2,
+            c['n']
+        )
