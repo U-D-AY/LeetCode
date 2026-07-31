3e51ffc712c785efbcfdc8d7a45aa6698777bf5b
U-D-AY <chilamkurthiudaysrinadh@gmail.com>
2026-07-31 21:47:22 +0530

    Time: 9 ms (94.22%), Space: 47.9 MB (73.7%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -1,19 +1,23 @@
 import java.util.Arrays;
+
 class Solution {
     public int minimumPushes(String word) {
         int[] arr = new int[26];
-
-        for(int i = 0; i < word.length(); i++) 
-            arr[word.charAt(i) - 'a']++;
-
         
+        // .getBytes() is highly optimized in modern Java for ASCII strings
+        for (byte b : word.getBytes()) {
+            arr[b - 'a']++;
+        }
+
         Arrays.sort(arr);
 
         int sol = 0;
-
-        for(int i=0; i<26; i++){
-            if (arr[25-i]==0) break;
-            sol = sol +  arr[25-i]*(i/8 + 1);
+        
+        // Iterate backwards directly to save the '25 - i' math on every step
+        for (int i = 25; i >= 0; i--) {
+            if (arr[i] == 0) break;
+            
+            sol += arr[i] * ((25 - i) / 8 + 1);
         }
 
         return sol;
